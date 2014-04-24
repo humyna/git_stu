1c9ac1dc72723738341523b863951caa61601cf6
humyna <hufo125@gmail.com>
2014-04-24 08:19:28 +0800

    new maven project add gitignore
    Signed-off-by: humyna <hufo125@gmail.com>

---
+++ gitdemo/src/main/java/info/zoio/tools/gitdemo/App.java
@@ -0,0 +1,13 @@
+package info.zoio.tools.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
